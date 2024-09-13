f097a8ac855f3f9063205bd684157fb256995d80
dikaputrawar <dikaputratjh@gmail.com>
2024-09-13 15:34:59 +0700

    first commit

---
+++ index.js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('new-message').addEventListener('click', function() {
+    document.getElementById('dialog-list').style.display = 'none';
+    document.getElementById('chat-screen').style.display = 'block';
+});
